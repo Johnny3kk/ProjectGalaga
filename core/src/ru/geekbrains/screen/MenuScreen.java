fed013860e87aed35c280ce803c66e5dc78eeb81
Jonatan Jonatanovich <jjnew@mail.ru>
2020-03-24 12:36:07 +0300

    HomeWork2

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -12,15 +12,16 @@ public class MenuScreen extends BaseScreen {
 
     private Texture img;
     private Vector2 pos;
-    private Vector2 v;
+    private Vector2 dist;
+
 //    private float rotate;
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
-        pos = new Vector2();
-        v = new Vector2(0.3f, 0.3f);
+        img = new Texture("fighter.png");
+        pos = new Vector2(0, 0);
+        dist = new Vector2();
 //        rotate = 0;
     }
 
@@ -39,20 +40,31 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        pos.set(screenX, Gdx.graphics.getHeight() - screenY);
+        dist.set(screenX, Gdx.graphics.getHeight() - screenY);
         return false;
     }
 
     private void update(float delta) {
-        pos.add(v);
-//        rotate += 1;
+            if (pos.x < dist.x) {
+                pos.x += dist.x * delta;
+            }
+            if (pos.x > dist.x){
+                pos.x -= dist.x * delta;
+            }
+            if (pos.y < dist.y) {
+                pos.y += dist.y * delta;
+            }
+            if (pos.y > dist.y) {
+                pos.y -= dist.y * delta;
+            }
+        //        rotate += 1;
     }
 
     private void draw() {
-        Gdx.gl.glClearColor(0.5f, 0.7f, 0.8f, 1);
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        batch.draw(img, pos.x, pos.y, 150, 100);
 //        batch.draw(new TextureRegion(img), pos.x, pos.y, pos.x, pos.y, 250, 250, 1, 1, rotate);
         batch.end();
     }
